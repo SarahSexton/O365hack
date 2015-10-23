8d998d674949be46771bc0659f71a6724df0f4d5
Mostafa Elzoghbi [MSFT] <moelzogh@microsoft.com>
2015-10-23 16:33:34 -0400

    changes

---
+++ O365Hackaton/DXNextHackatonWeb/Scripts/o365Integration.js
@@ -0,0 +1,28 @@
+﻿
+
+function createList(siteUrl) {
+    var clientContext = new SP.ClientContext(siteUrl);
+    var oWebsite = clientContext.get_web();
+
+    var listCreationInfo = new SP.ListCreationInformation();
+    listCreationInfo.set_title('Feedback');
+    listCreationInfo.set_templateType(SP.ListTemplateType.announcements);
+
+    this.oList = oWebsite.get_lists().add(listCreationInfo);
+
+    clientContext.load(oList);
+    clientContext.executeQueryAsync(
+        Function.createDelegate(this, this.onQuerySucceeded),
+        Function.createDelegate(this, this.onQueryFailed)
+    );
+}
+
+function onQuerySucceeded() {
+    var result = oList.get_title() + ' created.';
+    alert(result);
+}
+
+function onQueryFailed(sender, args) {
+    alert('Request failed. ' + args.get_message() +
+        '\n' + args.get_stackTrace());
+}
