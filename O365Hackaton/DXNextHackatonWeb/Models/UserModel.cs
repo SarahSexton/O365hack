06b180e212c91aa59e86cb7dc4e02fa4b40b9f27
Tomasz Wisniewski <tomasz.wisniewski@windowslive.com>
2015-10-23 18:50:44 +0200

    Added initial new project

---
+++ O365Hackaton/DXNextHackatonWeb/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DXNextHackatonWeb.Models
+{
+    //UserModel.cs
+    public class UserModel
+    {
+        public string displayName { get; set; }
+        public Guid objectId { get; set; }
+    }
+}
