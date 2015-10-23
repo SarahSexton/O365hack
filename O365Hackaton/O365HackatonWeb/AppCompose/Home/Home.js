2c889c51ada3707f7b6b5224a0055285c859badc
Mostafa Elzoghbi [MSFT] <moelzogh@microsoft.com>
2015-10-23 00:15:28 -0400

    o365 integration is done

--- O365Hackaton/O365HackatonWeb/AppCompose/Home/Home.js
+++ O365Hackaton/O365HackatonWeb/AppCompose/Home/Home.js
@@ -38,4 +38,37 @@
         }
     }
 
+    function addActivity() {
+
+        var siteUrl = '/TEReadinessHack';
+
+        function createListItem() {
+
+            var clientContext = new SP.ClientContext(siteUrl);
+            var oList = clientContext.get_web().get_lists().getByTitle('Activity');
+
+            var itemCreateInfo = new SP.ListItemCreationInformation();
+            this.oListItem = oList.addItem(itemCreateInfo);
+
+            oListItem.set_item('Title', 'My New Item!');
+            //oListItem.set_item('Body', 'Hello World!');
+
+            oListItem.update();
+
+            clientContext.load(oListItem);
+
+            clientContext.executeQueryAsync(Function.createDelegate(this, this.onQuerySucceeded), Function.createDelegate(this, this.onQueryFailed));
+        }
+
+        function onQuerySucceeded() {
+
+            alert('Item created: ' + oListItem.get_id());
+        }
+
+        function onQueryFailed(sender, args) {
+
+            alert('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+        }
+    }
+
 })();
